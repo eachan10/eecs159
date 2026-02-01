c5298606079d509332756263f21686f3aeca65eb
Eric Chan <eachan10@gmail.com>
2026-01-31 18:54:34 -0800

    add relu into linear

--- src/main.c
+++ src/main.c
@@ -19,8 +19,9 @@ void linear(int16_t *x, int16_t *w, int32_t *b, int16_t *out,
     acc += (1 << (shift - 1));  // rounding
     acc >>= shift;
     // quantize to uint8
-    acc = acc < -output_zero_point ? -output_zero_point : acc;   // ReLU
-    acc = acc > 255-output_zero_point ? 255-output_zero_point : acc;   // ReLU
+    // acc = acc < -output_zero_point ? -output_zero_point : acc;
+    acc = acc < 0 ? 0 : acc;  // ReLu
+    acc = acc > 255-output_zero_point ? 255-output_zero_point : acc;
     out[i] = acc;
   }
 }
@@ -36,11 +37,11 @@ void forward_pass(int16_t *x, int16_t *out) {
   int16_t buf1[512];
   int16_t buf2[512];
   linear(x, fc1_weights, fc1_bias, buf1, fc1_acc_scale, fc1_shift, fc1_output_zero_point, 624, 512);
-  relu(buf1, 512);
+  // relu(buf1, 512);
   linear(buf1, fc2_weights, fc2_bias, buf2, fc2_acc_scale, fc2_shift, fc2_output_zero_point, 512, 256);
-  relu(buf2, 256);
+  // relu(buf2, 256);
   linear(buf2, fc3_weights, fc3_bias, buf1, fc3_acc_scale, fc3_shift, fc3_output_zero_point, 256, 64);
-  relu(buf1, 64);
+  // relu(buf1, 64);
   linear(buf1, fc4_weights, fc4_bias, buf2, fc4_acc_scale, fc4_shift, fc4_output_zero_point, 64, 1);
   *out = buf2[0];
 }
